6867a432e934bc7106fb0a6da033fbf871196230
Alanda <151495503+Alanda@users.noreply.github.com>
2024-08-29 15:59:20 +0700

    week09

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import HomeStack2 from './navigations/HomeStack2';
 import BottomTab from './navigations/BottomTab';
 import RootStack from './navigations/RootStack';
+import ChartKitScreen from './screens/week5/ChartKitScreen';
 
 export default function App() {
     return (
@@ -11,5 +12,7 @@ export default function App() {
             {/* <BottomTab /> */}
             <RootStack />
         </NavigationContainer>
+        // <ChartKitScreen />
+
     );
 }
