bf7fde45732c72b2618ee2d31a5e3ed167be352c
Alanda <151495503+Alanda@users.noreply.github.com>
2024-09-12 16:08:01 +0700

    week10

---
+++ components/week10/UniversityMarkers.js
@@ -0,0 +1,21 @@
+import React from "react";
+import MapView, { Marker } from "react-native-maps";
+import { FontAwesome } from "@expo/vector-icons";
+
+export default function UniversityMarkers(props) {
+
+  return props.items.map((item) => (
+    <Marker
+      coordinate={{
+        "latitude": Number(item.latitude),
+        "longitude": Number(item.longitude),
+        "user_id": Number(item.id)
+      }}
+      title={item.user_id}
+      key={item.id.toString()}
+      description={item.address}
+    >
+        <FontAwesome name="user" size={20} color="tomato" />
+    </Marker>
+  ));
+}
